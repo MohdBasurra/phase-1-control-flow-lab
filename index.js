e25b6455c70b39d982ecf64e94a5aae35e6aae0b
MohdBasurra <mohdye96@gmail.com>
2022-12-27 19:45:41 +0300

    complete assigment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if(ride<=400){
+return('This one is on me!');
+  }
+
+
+else if(ride >400 && ride <=2000){
+  return('That will be twenty bucks.');
+}
+else if(ride>2500) {
+  return('No can do.');
+} 
+else if(ride>2000 ){
+  return ('I will gladly take your thirty bucks.');
+}
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(city){
+  return city== 'NYC' ?'Ok, sounds good.' :'No go.';
   // Write your code here!
+ 
+
+  
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case 'generous':
+      return'Thank you so much.';
+      case 'not as generous':
+        return 'Thank you.';
+        default :return"Bye.";
+  }
 }
